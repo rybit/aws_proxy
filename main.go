eded7c0539a346ce721f91a98bae7e1c9994ac94
Ryan Neal <ry.neal@gmail.com>
2016-06-13 09:51:06 -0700

    create almost working version

---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/crowdmob/goamz/aws"
+	"github.com/spf13/cobra"
+)
+
+var configFile string
+
+type configuration struct {
+	Port      int    `json:"port"`
+	SecretKey string `json:"secret_key"`
+	AccessKey string `json:"access_key"`
+	Region    string `json:"region"`
+	Service   string `json:"service"`
+	Endpoint  string `json:"endpoint"`
+}
+
+func main() {
+	root := cobra.Command{
+		Use: "es_proxy",
+		Run: start,
+	}
+	root.PersistentFlags().StringVarP(&configFile, "config", "c", "config.json", "config file to use")
+
+	if err := root.Execute(); err != nil {
+		panic(err)
+	}
+}
+
+func start(cmd *cobra.Command, args []string) {
+	config, err := loadFromFile(configFile)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %s", err)
+	}
+	region, ok := aws.Regions[config.Region]
+	if !ok {
+		keys := make([]string, 0, len(aws.Regions))
+		for k := range aws.Regions {
+			keys = append(keys, k)
+		}
+
+		log.Fatalf("Failed to parse the region: '%s', possible ones are %s", config.Region, strings.Join(keys, ","))
+	}
+
+	var auth aws.Auth
+	if config.SecretKey == "" && config.AccessKey == "" {
+		auth, err = aws.EnvAuth()
+		if err != nil {
+			log.Fatal("Failed to load the auth from the environment")
+		}
+	} else {
+		auth = aws.Auth{
+			SecretKey: config.SecretKey,
+			AccessKey: config.AccessKey,
+		}
+	}
+
+	signer := aws.NewV4Signer(auth, config.Service, region)
+
+	sign := func(w http.ResponseWriter, r *http.Request) {
+		proxyURL := fmt.Sprintf("http://%s%s", config.Endpoint, r.URL.RequestURI())
+
+		req, err := http.NewRequest(r.Method, proxyURL, r.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		//req.Header = r.Header
+		//delete(req.Header, "Cookie")
+
+		req.Header.Add("Date", time.Now().UTC().Format(time.RFC3339))
+		req.Header.Add("accept", "*/*")
+		signer.Sign(req)
+
+		fmt.Println("-----------")
+		for k, vs := range r.Header {
+			fmt.Printf("incoming: %s: %v\n", k, vs)
+		}
+
+		for k, vs := range req.Header {
+			fmt.Printf("outgoing: %s: %v\n", k, vs)
+		}
+		fmt.Println("-----------")
+
+		rsp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		defer rsp.Body.Close()
+		data, err := ioutil.ReadAll(rsp.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		_, err = w.Write(data)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+	http.HandleFunc("/", sign)
+	http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
+}
+
+func loadFromFile(filename string) (*configuration, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	config := new(configuration)
+	err = json.Unmarshal(data, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return validate(config)
+}
+
+func validate(config *configuration) (*configuration, error) {
+	if config.Endpoint == "" {
+		return nil, errors.New("Must provide an endpoint")
+	}
+	if config.Region == "" {
+		return nil, errors.New("Must provide a region")
+	}
+	if config.Service == "" {
+		return nil, errors.New("Must provide a region")
+	}
+	return config, nil
+}
